59421a62015762aa800c7ee3eca850c261a1a2a8
HungTrinh99 <minhhung.it99@gmail.com>
2019-09-28 20:47:37 +0700

    Edit SinhVien.js

--- Models/SinhVien.js
+++ Models/SinhVien.js
@@ -1,15 +1,1 @@
-// Hung sua
-function SinhVien() {
-    this.MaSV = '';
-    this.TenSV = '';
-<<<<<<< HEAD
-    this.Delete = function() {
-
-    }
-}
-=======
-}
-
-// hello
-//hello 2
->>>>>>> 8235bfa497622e2c79ab17b0cb3f30633f19089a
+// This is SinhVien propertypes
